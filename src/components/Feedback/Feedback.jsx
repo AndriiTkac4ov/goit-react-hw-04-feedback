544be390597499b27c6d3ac3c52a64a15265c8a1
AndriiTkac4ov <106906924+AndriiTkac4ov@users.noreply.github.com>
2023-01-14 23:45:58 +0200

    write solutions by hooks

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+const Feedback = () => {
+    // const [feedbacks, setFeedbacks] = useState ({
+    //     good: 0,
+    //     neutral: 0,
+    //     bad: 0,
+    // });
+
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
+
+    const handleCountGoodFeedbacks = () => {
+        setGood(prev => prev + 1);
+    }
+
+    const handleCountNeutralFeedbacks = () => {
+        setNeutral(prev => prev + 1);
+    }
+
+    const handleCountBadFeedbacks = () => {
+        setBad(prev => prev + 1);
+    }
+
+    const countTotalFeedback = () => {
+        return good + neutral + bad;
+    }
+
+    const countPositiveFeedbackPercentage = () => {
+        const positiveFeedbackPercentage = Math.round(good * 100 / countTotalFeedback());
+        return good ? positiveFeedbackPercentage : 0;
+    }
+
+    return (
+        <div className="feedback">
+            <h2 className="feedback_title">Please leave your feedback for us</h2>
+            <div className="feedback_table">
+                <button type='button' onClick={handleCountGoodFeedbacks}>Good</button>
+                <button type='button' onClick={handleCountNeutralFeedbacks}>Neutral</button>
+                <button type='button' onClick={handleCountBadFeedbacks}>Bad</button>
+            </div>
+            <h2 className="feedback_title">Statistics</h2>
+            <ul>
+                <li>Good: <span>{good}</span></li>
+                <li>Neutral: <span>{neutral}</span></li>
+                <li>Bad: <span>{bad}</span></li>
+                <li>Total: <span>{countTotalFeedback()}</span></li>
+                <li>Positive feedback: <span>{countPositiveFeedbackPercentage()}%</span></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Feedback;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from 'components/App';
+import Feedback from 'components/Feedback/Feedback';
+// import AppFeedback from 'components/AppFEEDBACK';
 import './index.css';
 import { theme } from './constants/theme';
 import { ThemeProvider } from 'styled-components';
@@ -8,7 +9,8 @@ import { ThemeProvider } from 'styled-components';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <Feedback />
+      {/* <AppFeedback /> */}
     </ThemeProvider>
   </React.StrictMode>
 );
